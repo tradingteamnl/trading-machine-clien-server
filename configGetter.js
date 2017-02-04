eb0d1b75fb79478706671bb5f4b67cb769beab6c
micheldaniel <micheldaniel2@hotmail.com>
2017-02-04 23:39:42 +0100

    bittrex.option
    bittrex.options zo gemaakt dat je het centraal kan opvragen

---
+++ configGetter.js
@@ -0,0 +1,28 @@
+//load modules
+var fs = require('fs');
+
+//laat filelocation
+var fileLocation = JSON.parse(fs.readFileSync("./temp/fileLocation.txt")).fileLocation;
+
+//laat config
+var config = JSON.parse(fs.readFileSync(fileLocation+"/config.json"));
+
+//getter voor de post url
+exports.postURL = function(){
+    return config.hostConnection.protocall+'://'+config.hostConnection.ip+':'+config.hostConnection.poort;
+};
+
+//bittrex request data
+exports.bittrexOptions = function(){
+    
+    //collect bittrex option
+    var bittrexOptions = ({
+        'apikey' : config.bittrex.apiKey,
+        'apisecret' : config.bittrex.apiSecretKey,
+        'stream' : config.bittrex.stream,
+        'verbose' : config.bittrex.verbose,
+        'cleartext' : config.bittrex.cleartext
+    });
+    
+    return bittrexOptions;
+};
